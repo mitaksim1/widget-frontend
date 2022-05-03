3fc86bb25740f4534dbf5d67a36a348932c7578a
Miriam <miriamt.simonnet@gmail.com>
2022-05-03 18:01:30 +0200

    Création bouton fermeture feedback

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,12 @@
+import { Popover } from '@headlessui/react';
+import { X } from 'phosphor-react';
+
+export function CloseButton() {
+    return (
+        // Popover.Button nous permet d'ouvrir le widget s'il est fermé et de le fermer s'il est ouvert, dans ce cas au clique sur le Popover il va le fermer
+        <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fermer formulaire de feedback">
+            <X weight="bold" className='w-4 h-4 '/>
+
+        </Popover.Button>
+    );
+}
--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,15 +1,18 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
     return (
         // w-[calc(100vw-2rem)] md:w-auto : aide à la responsivité de la page, si taille de l'écran à partir de médium laisser la taille auto sinon laisser une marge de 2rem
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Laisse ton feedback ici</span>
+                <CloseButton />
             </header>
 
             <p>Hello World</p>
 
             <footer className="text-xs text-neutral-400">
-                Fait par <a className="underline underline-offset-2" href="https://rpcketseat.com.br">Rocketseat</a>
+                Fait par <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
             </footer>
         </div>
     );
