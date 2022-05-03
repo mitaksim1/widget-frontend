4f13dac6fa3b31e565cb6788f6bf391f741cb047
Miriam <miriamt.simonnet@gmail.com>
2022-05-03 16:11:25 +0200

    Installation librairie headless-ui et impléméntation de Popover

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,17 +1,20 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
     return (
-        <div className="absolute bottom-5 right-5">
+        <Popover className="absolute bottom-5 right-5">
+            <Popover.Panel>Hello World</Popover.Panel>
+
             {/* group : permet de préciser que tous les éléments de button font partie d'un groupe */}
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className="w-6 h-6" />
-                {/* group-hover: permet d'ajouter un hover dans cet span grâce à la propriété group mis dans button */}
+                {/* group-hover: permet d'ajouter un hover dans cet span grâce à la propriété group mis dans Popover.Button */}
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
                     <span className='pl-2'></span>
                     Feedback
                 </span>
-            </button>
-        </div>       
+            </Popover.Button>
+        </Popover>       
     )
 }
